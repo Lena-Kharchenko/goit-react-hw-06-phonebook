4ead22a586e69ed0ef5a72fba27a7a91537f2d69
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-07-04 18:13:45 +0300

    Update contactFilter.jsx

--- src/components/contactFilter/contactFilter.jsx
+++ src/components/contactFilter/contactFilter.jsx
@@ -1,14 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { setStatusFilter } from 'redux/filterSlice';
 import PropTypes from 'prop-types';
 
-export default function ContactFilter({ onChange }) {
+export default function ContactFilter() {
+  const dispatch = useDispatch();
+
+  const handleFilterChange = e => {
+    dispatch(setStatusFilter(e.target.value));
+  };
+
   return (
     <>
       <p>Please enter the name </p>
-      <input type="text" name="filter" onChange={onChange}></input>
+      <input type="text" name="filter" onChange={handleFilterChange}></input>
     </>
   );
 }
 
 ContactFilter.propTypes = {
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
 };
