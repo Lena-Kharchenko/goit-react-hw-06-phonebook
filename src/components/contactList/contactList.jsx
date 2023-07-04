2968c7a2d16a887fa80ab53a5e634482acddfb53
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-07-04 18:16:04 +0300

    Update contactList.jsx

--- src/components/contactList/contactList.jsx
+++ src/components/contactList/contactList.jsx
@@ -1,18 +1,35 @@
+import { useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contactSlice';
+import { useDispatch } from 'react-redux';
+import Notiflix from 'notiflix';
 import PropTypes from 'prop-types';
 import css from './contactList.module.css';
 
-export default function ContactList({ contactList, onChange }) {
+export default function ContactList() {
+  const contactsRedux = useSelector(state => state.contacts);
+  const filterRedux = useSelector(state => state.filters);
+  const dispatch = useDispatch();
+
+  const onDeleteContact = e => {
+    dispatch(deleteContact(e.target.id));
+    Notiflix.Notify.success(`The contact has been successfully deleted`);
+  };
+
+  const visibleContacts = filterRedux
+    ? contactsRedux.filter(contact => contact.name.includes(filterRedux))
+    : contactsRedux;
+
   return (
     <>
-      {contactList.length === 0 ? (
+      {visibleContacts.length === 0 ? (
         <div>Empty</div>
       ) : (
         <ul>
-          {contactList.map(({ name, id, number }) => (
+          {visibleContacts.map(({ name, id, number }) => (
             <li key={id}>
               <span className={css.name}> {name}:</span>
               <span className={css.number}>{number}</span>
-              <button type="button" id={id} onClick={onChange}>
+              <button type="button" id={id} onClick={onDeleteContact}>
                 Delete
               </button>
             </li>
@@ -31,5 +48,5 @@ ContactList.propTypes = {
     })
   ),
 
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
 };
