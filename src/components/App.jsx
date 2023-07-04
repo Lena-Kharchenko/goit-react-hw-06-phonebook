0936706eb281ee129920447230e17d08b777ecf7
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-07-04 18:13:28 +0300

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,59 @@
-import { useState, useEffect, useMemo } from 'react';
+// import { useState, useEffect, useMemo } from 'react';
 import ContactForm from './contactForm/contactForm';
 import ContactList from './contactList/contactList';
 import ContactFilter from './contactFilter/contactFilter';
-import Notiflix from 'notiflix';
-import contactsList from 'savedContactList/savedContactList';
+// import Notiflix from 'notiflix';
+// import contactsList from 'savedContactList/savedContactList';
 
 export default function App() {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(window.localStorage.getItem('contacts')) ?? contactsList
-  );
+  // const [contacts, setContacts] = useState(
+  //   () => JSON.parse(window.localStorage.getItem('contacts')) ?? contactsList
+  // );
 
-  const [filter, setFilter] = useState('');
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const handleChangeFilter = event => {
-    setFilter(event.target.value);
-  };
+  // const handleChangeFilter = event => {
+  //   setFilter(event.target.value);
+  // };
 
-  const addContacts = (id, name, number) => {
-    if (contacts.find(contact => contact.name === name) !== undefined) {
-      Notiflix.Notify.failure(`${name} is already in your contact book`);
-      return;
-    }
+  // const addContacts = (id, name, number) => {
+  //   if (contacts.find(contact => contact.name === name) !== undefined) {
+  //     Notiflix.Notify.failure(`${name} is already in your contact book`);
+  //     return;
+  //   }
 
-    setContacts(prevState => [{ id, name, number }, ...prevState]);
-    Notiflix.Notify.success(`You add ${name} to your phonebook`);
-  };
+  //   setContacts(prevState => [{ id, name, number }, ...prevState]);
+  //   Notiflix.Notify.success(`You add ${name} to your phonebook`);
+  // };
 
-  const visibleContacts = useMemo(() => {
-    const filterNormalize = filter.toLowerCase();
-    const visibleContacts = filter
-      ? contacts.filter(contact =>
-          contact.name.toLocaleLowerCase().includes(filterNormalize)
-        )
-      : contacts;
-    return visibleContacts;
-  }, [contacts, filter]);
+  // const visibleContacts = useMemo(() => {
+  //   const filterNormalize = filter.toLowerCase();
+  //   const visibleContacts = filter
+  //     ? contacts.filter(contact =>
+  //         contact.name.toLocaleLowerCase().includes(filterNormalize)
+  //       )
+  //     : contacts;
+  //   return visibleContacts;
+  // }, [contacts, filter]);
 
-  const handleDeleteContact = event => {
-    const deleteSelectContact = contacts.filter(
-      contact => contact.id !== event.target.id
-    );
+  // const handleDeleteContact = event => {
+  //   const deleteSelectContact = contacts.filter(
+  //     contact => contact.id !== event.target.id
+  //   );
 
-    setContacts([...deleteSelectContact]);
-  };
+  //   setContacts([...deleteSelectContact]);
+  // };
 
   return (
     <div>
       <h1>My Phonebook</h1>
-      <ContactForm onSubmit={addContacts} />
-      <ContactFilter onChange={handleChangeFilter} value={filter} />
-      <ContactList
-        contactList={visibleContacts}
-        onChange={handleDeleteContact}
-      />
+      <ContactForm />
+      <ContactFilter />
+      <ContactList />
     </div>
   );
 }
